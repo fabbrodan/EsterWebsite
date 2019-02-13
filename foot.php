05237d71a499eda95f1f0aabfa56396c589b30ec
fabbrodan <daniel.aaslund@gmail.com>
2019-02-13 19:16:15 +0100

    commit

--- foot.php
+++ foot.php
@@ -8,7 +8,6 @@ session_start();
 
 <footer class="footer">
     <div class="container">
-<<<<<<< HEAD
         <nav class="navbar navbar-expand navbar-light bg-light fixed-bottom">
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav mr-auto">
@@ -23,22 +22,6 @@ session_start();
                     if (isset($_SESSION["user"])) {
                         echo "<li class='nav-item'><a class='nav-link' href='/upload.php'>Upload<span class='sr-only'></span></a></li>";
                         echo "<li class='nav-item'><a class='nav-link' href='/logout.php'>Logout<span class='sr-only'></span></a></li>";
-=======
-        <nav class="navbar navbar-expand bg-light fixed-bottom justify-content-center">
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href ="/index.php">Home<span class="sr-only"></span></a>
-                    </li>
-                    <?php
-                    if (!isset($_SESSION["user"])) {
-                        echo "<li class='nav-item'><a class='nav-link text-body' href='/login.php'>Login<span class='sr-only></span></a></li>";
-
-                    }
-                    if (isset($_SESSION["user"])) {
-                        echo "<li class='nav-item'><a class='nav-link text-body' href='/upload.php'>Upload<span class='sr-only'></span></a></li>";
-                        echo "<li class='nav-item'><a class='nav-link text-body' href='/logout.php'>Logout<span class='sr-only'></span></a></li>";
->>>>>>> ff2c2ebc3ad688b528eabc4f4634212506cb8df9
                     }
                     ?>
                 </ul>
